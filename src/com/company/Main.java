a386dd62e8ab1db9d919c6601fb0838ed411e639
Morgan <melon21@hotmail.se>
2020-11-02 12:05:23 +0100

    TYIfactor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -20,6 +22,7 @@ public class Main {
     //https://www.tutorialspoint.com/java_xml/java_sax_parse_document.htm : adjust test code to your needs
     //set a killTimer to how frequently the prognosis will update
 
+    Timer timer;
     long lifespan;
 
     public static void main(String[] args) {
@@ -27,17 +30,18 @@ public class Main {
     }
 }
 
-    class UserHandler extends DefaultHandler {
+class UserHandler extends DefaultHandler {
         String from, to;
+
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-            if(qName.equalsIgnoreCase("time")){
+            if (qName.equalsIgnoreCase("time")) {
                 from = attributes.getValue(1).substring(11, 13);
                 to = attributes.getValue(2).substring(11, 13);
                 if (from.equals(to)) return;
                 System.out.print("kl. " + from + " - " + to);
             }
-            if(qName.equalsIgnoreCase("temperature")){
+            if (qName.equalsIgnoreCase("temperature")) {
                 System.out.println("\t" + attributes.getValue(2) + " C");
             }
             super.startElement(uri, localName, qName, attributes);
@@ -45,6 +49,10 @@ public class Main {
     }
 
 class GUI implements ActionListener {
+    long lifespan;
+    Timer timer = new Timer();
+    TimerTask timerTask;
+
     JFrame frame;
 
     JComboBox<String> Cities;
@@ -53,8 +61,9 @@ class GUI implements ActionListener {
     JLabel desc;
 
     JPanel panel;
-    JTextField manual;
+    JTextField hour;
     JLabel temperature;
+
 
 
     public GUI() {
@@ -63,7 +72,7 @@ class GUI implements ActionListener {
 
         desc = new JLabel("update time in minutes: ");
 
-        //get killTimer value
+        //get lifespan value
         lifeTime = new JTextField();
         lifeTime.addActionListener(this);
 
@@ -72,28 +81,28 @@ class GUI implements ActionListener {
         Cities.addItem("Skelleftea");
         Cities.addItem("Kage");
         Cities.addItem("Stockholm");
-        manual = new JTextField();
-        manual.addActionListener(this);
+        hour = new JTextField();
+        hour.addActionListener(this);
 
         //get prognosis
         submit = new JButton("get prognosis");
         submit.addActionListener(this);
-        temperature = new JLabel("temperature: ");
+        temperature = new JLabel("temperature in ");
 
 
         //panel to hold all content of the GUI
         panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        panel.setLayout(new GridLayout(3, 3));
+        panel.setLayout(new GridLayout(4, 3));
 
         panel.add(desc);        // row 0 cols 0
         panel.add(lifeTime);    // row 0 cols 1
 
         panel.add(Cities);      // row 1 cols 0
-        panel.add(manual);      // row 1 cols 1
+        panel.add(hour);        // row 1 cols 1
         panel.add(submit);      // row 1 cols 2
 
-        panel.add(temperature);
+        panel.add(temperature); // row 2 cols 0
 
         //the GUI mainframe
         frame.add(panel, BorderLayout.CENTER);
@@ -105,21 +114,19 @@ class GUI implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        timer.schedule(timerTask, lifespan);
         try {
-            ParseXml(getURLStream());
+                ParseXml(getURLStream());
         } catch (ParserConfigurationException | SAXException | IOException Error) {
             Error.printStackTrace();
         }
     }
 
-
-    //the stream received from the URL
     InputStream getURLStream() throws IOException {
-        //get longitude and latitude for selected city
+        float lat, lon; lat = lon = 0f;
         HttpURLConnection huc;
         InputStream website;
-        float lat = 0f;
-        float lon = 0f;
+
         System.out.println(Cities.getItemAt(Cities.getSelectedIndex()) + ": ");
         switch (Cities.getItemAt(Cities.getSelectedIndex())) {
             case "Skelleftea" -> {
